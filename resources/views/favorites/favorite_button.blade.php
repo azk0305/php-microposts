9ecebc882a5b1f5994096c75a2f77ffa58edca98
Kenji Azami <k.azami0305@gmail.com>
2018-10-04 05:30:19 +0000

    adjust favorite url

--- resources/views/favorites/favorite_button.blade.php
+++ resources/views/favorites/favorite_button.blade.php
@@ -1,9 +1,9 @@
 @if (Auth::user()->is_favorite($micropost->id))
-    {!! Form::open(['route' => ['user.unfavorite', $micropost->id], 'method' => 'delete']) !!}
+    {!! Form::open(['route' => ['favorites.unfavorite', $micropost->id], 'method' => 'delete']) !!}
         {!! Form::submit('Unfavorite', ['class' => "btn btn-success btn-sm"]) !!}
     {!! Form::close() !!}
 @else
-    {!! Form::open(['route' => ['user.favorite', $micropost->id]]) !!}
+    {!! Form::open(['route' => ['favorites.favorite', $micropost->id]]) !!}
         {!! Form::submit('Favorite', ['class' => "btn btn-light btn-sm"]) !!}
     {!! Form::close() !!}
 @endif
